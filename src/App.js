f7de425168abba7f614a8395e7ceb4019bfaaa14
LuigiMagdamitUCLA <luigiantonmagdamit@gmail.com>
2022-07-20 10:15:04 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import mp3 from './audio/test.mp3'
+import ReactAudioPlayer from 'react-audio-player';
+import songs from './audio/index'
+import React from 'react';
 
+
+
+console.log(songs)
+
+
+
+class Radio extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props.song)
+  }
+  render() {
+    return (
+      <div>
+       <div class = "player">
+        <div class = "top">
+          <h1>{this.props.song.name}</h1>
+          <h1>{this.props.song.pcn}</h1>
+        </div>
+        <ReactAudioPlayer
+          src={this.props.song.mp3}
+          autoPlay
+          controls
+          style={{width : "500px"}}
+        />
+      </div>
+      </div>
+    );
+  }
+}
+
+class Window extends React.Component {
+  constructor(props) {
+    super(props);
+    
+
+    let songnames = Object.values(songs)
+    const songList = songnames.map((song) => 
+      <li class = "song" key = {song.name}><button onClick={() => this.handleClick(song)} value = {song.mp3}>{song.name}</button></li>
+    );
+
+    this.state = {songStore: songnames, songList: songList, currentSong: songnames[0]}
+  }
+  handleClick(info) {
+    console.log(this.state.currentSong)
+    this.state.currentSong = info
+    this.forceUpdate();
+
+  }
+  render() {
+    return(<div>
+      <h1>Samahang Pilipino Cultural Night Radio</h1>
+      <Radio song={this.state.currentSong}/>
+      <h1>Song List</h1>
+     <ul>{this.state.songList}</ul>
+    </div>)
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class = "main">
+      
+      <Window />
     </div>
+    
   );
 }
 
---
+++ src/audio/index.js
@@ -0,0 +1,61 @@
+import s1 from './test.mp3';
+import s2 from './01 Overture.mp3';
+import s3 from './02 After Tonight.mp3'
+import s4 from './03 Mother May I - My Way Home.mp3'
+import s5 from './04 There With You.mp3'
+import s6 from './05 Where Are You.mp3'
+
+import s9 from './08 Cross That Bridge.mp3'
+const song1 = {
+    name: "Since the Day(Live)",
+    pcn: "SPCN 1996",
+    mp3: s1
+}
+
+const song2 = {
+    name: "Overture",
+    pcn: "SPCN 1996",
+    mp3: s2
+}
+
+const song3 = {
+    name: "After Tonight",
+    pcn: "SPCN 1996",
+    mp3: s3
+}
+
+const song4 = {
+    name: "Mother May I - My Way Home",
+    pcn: "SPCN 1996",
+    mp3: s4
+}
+
+const song5 = {
+    name: "There With You",
+    pcn: "SPCN 1996",
+    mp3: s5
+}
+
+const song6 = {
+    name: "Where Are You",
+    pcn: "SPCN 1996",
+    mp3: s6
+}
+
+const song9 = {
+    name: "Cross That Bridge",
+    pcn: "SPCN 1996",
+    mp3: s9
+}
+
+const songs = {
+    song1: song1,
+    song2: song2,
+    song3: song3,
+    song4: song4,
+    song5: song5,
+    song6: song6,
+    song9, song9
+
+}
+export default songs
